42099bf6c771be39c0094387889261fd5495e9a3
dhruv aggarwal <dhruvgwal@gmail.com>
2025-10-10 00:23:09 +0530

    checker

--- src/lib/auth/AuthContext.tsx
+++ src/lib/auth/AuthContext.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+export const dynamic = "force-dynamic";
 import { createContext, useContext, useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { AuthService } from "../supabase/auth";
